5e5a52641a5a762321fb3bf1c9dfc652a54c8a81
vattcarter7 <vattcarter@gmail.com>
2020-07-27 20:31:57 +0700

    implemented EventList EventListAttendee and EventListItem

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import EventListItem from './EventListItem';
+
+const EventList = () => {
+  return (
+    <Fragment>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </Fragment>
+  );
+};
+
+export default EventList;
