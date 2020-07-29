df7e83745f49f9ffe3cab50500715d237cf9bb4d
vattcarter7 <vattcarter@gmail.com>
2020-07-29 13:42:55 +0700

    added date-fns

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -4,6 +4,7 @@ import EventListAttendee from './EventListAttendee';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { deleteEvent } from '../eventActions';
+import { format } from 'date-fns';
 
 const EventListItem = ({ event }) => {
   const dispatch = useDispatch();
@@ -22,7 +23,7 @@ const EventListItem = ({ event }) => {
       </Segment>
       <Segment>
         <span>
-          <Icon name='clock' /> {event.date}
+          <Icon name='clock' /> {format(event.date, 'MMMM d, yyyy h:mm a')}
           <Icon name='marker' /> {event.venue}
         </span>
       </Segment>
