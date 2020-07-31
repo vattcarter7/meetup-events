219ba277052506075353773718a11fa66e917d8f
vattcarter7 <vattcarter@gmail.com>
2020-07-31 14:45:34 +0700

    handled auth errors

--- src/features/auth/RegisterForm.jsx
+++ src/features/auth/RegisterForm.jsx
@@ -3,13 +3,10 @@ import ModalWrapper from '../../app/common/modals/ModalWrapper';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../app/common/form/MyTextInput';
-import { Button } from 'semantic-ui-react';
+import { Button, Label } from 'semantic-ui-react';
 import { useDispatch } from 'react-redux';
 import { closeModal } from '../../app/common/modals/modalReducer';
-import {
-  signInWithEmail,
-  registerInFirebase
-} from '../../app/firestore/firebaseService';
+import { registerInFirebase } from '../../app/firestore/firebaseService';
 
 export default function RegisterForm() {
   const dispatch = useDispatch();
@@ -23,18 +20,18 @@ export default function RegisterForm() {
           email: Yup.string().required().email(),
           password: Yup.string().required()
         })}
-        onSubmit={async (values, { setSubmitting }) => {
+        onSubmit={async (values, { setSubmitting, setErrors }) => {
           try {
             await registerInFirebase(values);
             setSubmitting(false);
             dispatch(closeModal());
           } catch (error) {
+            setErrors({ auth: error.message });
             setSubmitting(false);
-            console.log(error);
           }
         }}
       >
-        {({ isSubmitting, isValid, dirty }) => (
+        {({ isSubmitting, isValid, dirty, errors }) => (
           <Form className='ui form'>
             <MyTextInput name='displayName' placeholder='Display Name' />
             <MyTextInput name='email' placeholder='Email Address' />
@@ -43,6 +40,14 @@ export default function RegisterForm() {
               placeholder='Password'
               type='password'
             />
+            {errors.auth && (
+              <Label
+                basic
+                color='red'
+                style={{ marginBottom: 10 }}
+                content={errors.auth}
+              />
+            )}
             <Button
               loading={isSubmitting}
               disabled={!isValid || !dirty || isSubmitting}
