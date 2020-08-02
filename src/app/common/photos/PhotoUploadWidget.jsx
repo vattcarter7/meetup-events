ee62e2b121ca0b08f50f75c7f966fd72f1b5adda
vattcarter7 <vattcarter@gmail.com>
2020-08-02 12:59:54 +0700

    implemented react-cropper

--- src/app/common/photos/PhotoUploadWidget.jsx
+++ src/app/common/photos/PhotoUploadWidget.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import { Grid, Header } from 'semantic-ui-react';
+import React, { useState, Fragment } from 'react';
+import { Grid, Header, Button } from 'semantic-ui-react';
+import PhotoWidgetDropzone from './PhotoWidgetDropzone';
+import PhotoWidgetCropper from './PhotoWidgetCropper';
 
 const PhotoUploadWidget = () => {
+  const [files, setFiles] = useState([]);
+  const [image, setImage] = useState(null);
   return (
     <Grid>
       <Grid.Column width={4}>
         <Header color='teal' sub content='Step 1 - Add Photo' />
+        <PhotoWidgetDropzone setFiles={setFiles} />
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
         <Header color='teal' sub content='Step 2 - Resize' />
+        {files.length > 0 && (
+          <PhotoWidgetCropper
+            setImage={setImage}
+            imagePreview={files[0].preview}
+          />
+        )}
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
         <Header color='teal' sub content='Step 3 - Preview & upload' />
+        {files.length > 0 && (
+          <Fragment>
+            <div
+              className='img-preview'
+              style={{ minHeight: 200, minWidth: 200, overflow: 'hidden' }}
+            />
+            <Button.Group>
+              <Button style={{ width: 100 }} positive icon='check' />
+              <Button style={{ width: 100 }} icon='close' />
+            </Button.Group>
+          </Fragment>
+        )}
       </Grid.Column>
     </Grid>
   );
